f13be79a40b02b9bae1ae04d5f79b2d2c7740f54
Sale1996 <ftnmejl96@gmail.com>
2021-03-09 11:14:57 +0100

    Code refactor inside data processor sub module of data set loader module

--- automatization_of_data_mining_project/data_set_loader/classes/pandas_data_set_loader/processor/converter/document_converter/document_column_pair_converter.py
+++ automatization_of_data_mining_project/data_set_loader/classes/pandas_data_set_loader/processor/converter/document_converter/document_column_pair_converter.py
@@ -3,63 +3,67 @@ import pandas
 from data_set_loader.classes.pandas_data_set_loader.loader.data_loader import DataLoader
 from data_set_loader.classes.pandas_data_set_loader.processor.converter.column_pair_converter import ColumnPairConverter
 
-'''
-
-UKLONI COUNTRY CODE I COUNTRY NAME IZ NAZIVA VARIABLI, NAPRAVI GA VISE GENERALIZOVANIM!
-
-'''
 
 class DocumentColumnPairConverter(ColumnPairConverter):
 
     def __init__(self, loader: DataLoader):
         self.loader = loader
 
-    def convert_values_of_changeable_column_to_match_important_column(self, loaded_data, pair_important_column, pair_changeable_column):
-        countries_data_set = self.load_countries_data_set(pair_important_column, pair_changeable_column)
+    def convert_values_of_changeable_column_to_match_important_column(
+            self, loaded_data, pair_important_column, pair_changeable_column):
 
-        country_name_to_country_code_map = self.get_country_name_to_country_code_map(countries_data_set)
+        converter_data_set = self.load_converter_data_set(pair_important_column, pair_changeable_column)
+        changeable_column_to_important_column_map = self.get_changeable_column_to_important_column_map(
+            converter_data_set)
+        updated_data_frame = self.change_changeable_column_to_important_column_in_data_set(
+            changeable_column_to_important_column_map, loaded_data, pair_important_column, pair_changeable_column)
 
-        updated_data_frame = self.change_country_name_to_country_code_in_data_set(country_name_to_country_code_map,
-                                                                                  loaded_data)
         return updated_data_frame
 
-    def change_country_name_to_country_code_in_data_set(self, country_name_to_country_code_map, loaded_data):
-        country_name_index = loaded_data.columns.get_loc("Country Name")
-        new_columns = self.change_country_name_column_name_to_country_code(country_name_index, loaded_data)
+    def change_changeable_column_to_important_column_in_data_set(self, changeable_column_to_important_column_map,
+                                                                 loaded_data, important_column,
+                                                                 changeable_column):
 
-        updated_data_set = self.change_country_name_fields_to_country_code(country_name_index,
-                                                                           country_name_to_country_code_map,
-                                                                           loaded_data)
-
+        changeable_column_index = loaded_data.columns.get_loc(changeable_column)
+        new_columns = self.change_changeable_column_name_to_important_column_name(
+            changeable_column_index, loaded_data, important_column)
+        updated_data_set = self.convert_values_of_changeable_column_into_corresponding_values_of_important_column(
+            changeable_column_index, changeable_column_to_important_column_map, loaded_data)
         updated_data_frame = pandas.DataFrame(updated_data_set, columns=new_columns)
+
         return updated_data_frame
 
-    def change_country_name_fields_to_country_code(self, country_name_index, country_name_to_country_code_map,
-                                                   loaded_data):
-        # Add it only if there is Country code for country name!
+    def convert_values_of_changeable_column_into_corresponding_values_of_important_column(
+            self, changeable_column_index, changeable_column_to_important_column_map, loaded_data):
+
+        # Add it only if map contains important column value for changeable column key value
         updated_data_set = []
         for row in loaded_data.values:
-            if row[country_name_index] in country_name_to_country_code_map:
+            if row[changeable_column_index] in changeable_column_to_important_column_map:
                 new_row = row
-                new_row[country_name_index] = country_name_to_country_code_map[row[country_name_index]]
+                new_row[changeable_column_index] = changeable_column_to_important_column_map[row[changeable_column_index]]
                 updated_data_set.append(new_row)
+
         return updated_data_set
 
-    def change_country_name_column_name_to_country_code(self, country_name_index, loaded_data):
+    def change_changeable_column_name_to_important_column_name(
+            self, changeable_column_index, loaded_data, pair_important_column):
+
         new_columns = loaded_data.columns.tolist()[:]
-        new_columns[country_name_index] = "Country Code"
+        new_columns[changeable_column_index] = pair_important_column
+
         return new_columns
 
-    def get_country_name_to_country_code_map(self, iso3_data_values):
-        country_name_to_country_code_map = {}
-        for country_value in iso3_data_values:
-            country_name_to_country_code_map[country_value[1]] = country_value[0]
-        return country_name_to_country_code_map
+    def get_changeable_column_to_important_column_map(self, converter_data_set):
+        changeable_column_to_important_column_map = {}
+        for converter_value in converter_data_set:
+            changeable_column_to_important_column_map[converter_value[1]] = converter_value[0]
+        return changeable_column_to_important_column_map
 
-    def load_countries_data_set(self, pair_important_column, pair_changeable_column):
+    def load_converter_data_set(self, pair_important_column, pair_changeable_column):
         document_name = pair_important_column + '_' + pair_changeable_column
         document_name = document_name.replace(' ', '_')
-        loaded_iso3_data = self.loader.load_data(
+        loaded_converter_data = self.loader.load_data(
             'C:/Users/Sale/Desktop/MASTER_PROJEKAT/automatization_of_data_mining_project/automatization_of_data_mining_project/data_set_loader/classes/pandas_data_set_loader/processor/converter/document_converter/data/' + document_name + '.xlsx')
-        iso3_data_values = loaded_iso3_data[['Country Code', 'Country Name']].values
-        return iso3_data_values
+        converter_data_set = loaded_converter_data[[pair_important_column, pair_changeable_column]].values
+        return converter_data_set
--- automatization_of_data_mining_project/data_set_loader/classes/pandas_data_set_loader/processor/data_processor_for_pair_of_must_contained_columns.py
+++ automatization_of_data_mining_project/data_set_loader/classes/pandas_data_set_loader/processor/data_processor_for_pair_of_must_contained_columns.py
@@ -1,6 +1,11 @@
 from data_set_loader.classes.pandas_data_set_loader.processor.converter.column_pair_converter import ColumnPairConverter
 from data_set_loader.classes.pandas_data_set_loader.processor.data_processor import DataProcessor
 
+'''
+  Column which needs to be transformed into other pair 'pair_important_column'
+  for example 'Country Name' is changeable column and 'Country Code' is important column
+'''
+
 
 class DataProcessorForPairOfMustContainedColumns(DataProcessor):
 
@@ -9,41 +14,39 @@ class DataProcessorForPairOfMustContainedColumns(DataProcessor):
 
     def update_data_set_and_must_contained_columns(self, loaded_data, must_contained_columns,
                                                    pairs_of_must_contained_columns):
-        updated_data_set = []
+        updated_data_set = loaded_data
         updated_must_contained_columns = must_contained_columns
+
         for pair_of_must_contained_columns in pairs_of_must_contained_columns:
             pair_important_column = pair_of_must_contained_columns[0]
-            # column which needs to be transformed into other pair 'pair_important_column'
-            # for example 'Country Name' is changeable column and 'Country Code' is important column
             pair_changeable_column = pair_of_must_contained_columns[1]
 
-            updated_must_contained_columns = self.add_country_code_to_must_contained_columns_array(updated_must_contained_columns, pair_important_column)
+            updated_must_contained_columns = self.add_important_column_to_must_contained_columns_array(
+                updated_must_contained_columns, pair_important_column)
 
-            if not self.is_contained_in_data(loaded_data, pair_changeable_column):
-                return loaded_data, updated_must_contained_columns
+            if not self.is_contained_in_data(updated_data_set, pair_changeable_column):
+                continue
 
-            if self.is_contained_in_data(loaded_data, pair_important_column):
-                data_set_without_changeable_column = self.remove_changeable_column_from_data_set(loaded_data, pair_changeable_column)
-                return data_set_without_changeable_column, updated_must_contained_columns
+            if self.is_contained_in_data(updated_data_set, pair_important_column):
+                updated_data_set = self.remove_changeable_column_from_data_set(updated_data_set, pair_changeable_column)
+                continue
 
-            updated_data_set = self.column_pair_converter.convert_values_of_changeable_column_to_match_important_column\
-                (loaded_data, pair_important_column, pair_changeable_column)
+            updated_data_set = self.column_pair_converter.convert_values_of_changeable_column_to_match_important_column \
+                (updated_data_set, pair_important_column, pair_changeable_column)
 
         return updated_data_set, updated_must_contained_columns
 
+    def add_important_column_to_must_contained_columns_array(self, must_contained_columns, additional_important_column):
+        updated_must_contained_columns = must_contained_columns[:]
+        updated_must_contained_columns.append(additional_important_column)
 
+        return updated_must_contained_columns
+
+    def is_contained_in_data(self, loaded_data, column_name):
+        return column_name in loaded_data.columns.tolist()
 
     def remove_changeable_column_from_data_set(self, loaded_data, pair_changeable_column):
         columns_without_changeable_column = loaded_data.columns.tolist()[:]
         columns_without_changeable_column.remove(pair_changeable_column)
         data_set_without_changeable_column = loaded_data[columns_without_changeable_column]
         return data_set_without_changeable_column
-
-    def is_contained_in_data(self, loaded_data, column_name):
-        return column_name in loaded_data.columns.tolist()
-
-    def add_country_code_to_must_contained_columns_array(self, must_contained_columns, additional_important_column):
-        updated_must_contained_columns = must_contained_columns[:]
-        updated_must_contained_columns.append(additional_important_column)
-
-        return updated_must_contained_columns
